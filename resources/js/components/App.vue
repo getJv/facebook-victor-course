3196bd4cd5c7d8495d347fe9bb4496e5a548e404
Jhonatan Morais <jhonatanvinicius@gmail.com>
2019-12-15 15:44:29 -0300

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>App Component 
+      <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: "App"
+};
+</script>
---
+++ resources/js/views/Start.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Start viewee
+    </div>
+</template>
+<script>
+export default {
+    name:'start'
+}
+</script>
