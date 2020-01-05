458bf682bc0084bff098ffdc3f9eccf6a2537ae6
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-01-05 23:57:05 +0000

    fim da aula 10

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,18 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Resources\Post as PostResorce;
+
+use App\Post;
+use App\Http\Resources\Post as PostResource;
+use App\Http\Resources\PostCollection as PostCollection;
 
 class PostController extends Controller
 {
+
+    public function index()
+    {
+        return new PostCollection(Post::all());
+    }
+
     public function store()
     {
         $data = request()->validate([
-            'data.attributes.body' => ''
+            'data.attributes.body' => '',
         ]);
 
         $post = request()->user()->posts()->create($data['data']['attributes']);
 
-        return new PostResorce($post);
+        return new PostResource($post);
     }
 }
---
+++ tests/Feature/RetrievePostTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Post;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RetrievePostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_retrieve_posts()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs($user = factory(User::class)->create(),'api');
+        $posts = factory(Post::class, 2)->create();
+        $response = $this->get('/api/posts');
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    [
+                        'data' => [
+                            'type' => 'posts',
+                            'post_id' => $posts->first()->id,
+                            'attributes' => [
+                                'body' => $posts->first()->body,
+                            ],
+                        ],
+                    ],
+                    [
+                        'data' => [
+                            'type' => 'posts',
+                            'post_id' => $posts->last()->id,
+                            'attributes' => [
+                                'body' => $posts->last()->body,
+                            ],
+                        ],
+                    ],
+                ],
+                'links' => [
+                    'self' => url('/posts'),
+                ]
+
+            ]);
+
+    }
+
+}
