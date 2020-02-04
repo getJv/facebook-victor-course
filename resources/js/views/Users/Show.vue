753e0545b53c6197bb16bdc6faaab2412e0e8d4a
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-02-04 00:05:31 -0300

    aula 24 ok

--- resources/js/views/Users/Show.vue
+++ resources/js/views/Users/Show.vue
@@ -1,18 +1,43 @@
 <template>
-  <div>
-    <div class="w-11 h-64 overflow-hidden">
-      <img src alt="userbg image" class="object-cover w-full" />
+  <div class="flex flex-col items-center">
+    <div class="relative mb-8">
+      <div class="w-100 h-64 overflow-hidden z-10">
+        <img
+          src="https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1008"
+          alt="userbg image"
+          class="object-cover w-full"
+        />
+      </div>
+      <div class="absolute flex items-center bottom-0 left-0 -mb-8 ml-12 z-20">
+        <div class="w-32">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg"
+            alt="user profile"
+            class="object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg"
+          />
+        </div>
+        <p class="text-2xl text-gray-100 ml-4">{{user.data.attributes.name }}</p>
+      </div>
     </div>
+
+    <p v-if="postLoading">Loading posts...</p>
+    <Post v-else v-for="post in posts.data" :key="post.data. post_id" :post="post" />
+    <p v-if="!postLoading && posts.data.length < 1 ">No posts found. Get Started....</p>
   </div>
 </template>
 <script>
+import Post from "../../components/Post";
 export default {
   name: "Show",
+  components: {
+    Post
+  },
   data: () => {
     return {
       user: null,
       posts: null,
-      loading: true
+      userLoading: true,
+      postLoading: true
     };
   },
   mounted() {
@@ -25,16 +50,18 @@ export default {
         console.log(err);
       })
       .finally(() => {
-        this.loading = false;
+        this.userLoading = false;
       });
     axios
-      .get("/api/posts/" + this.$route.params.userId)
+      .get("/api/users/" + this.$route.params.userId + "/posts")
       .then(res => {
         this.posts = res.data;
       })
       .catch(err => {
         console.log("Unable to facth posts");
-        this.loading = !this.loading;
+      })
+      .finally(() => {
+        this.postLoading = false;
       });
   }
 };
