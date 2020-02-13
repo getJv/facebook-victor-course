7267400b1aa09f89fe96c38c3c20d6cda25f36d4
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-02-13 02:04:51 +0000

    aula 29 ok

---
+++ app/Http/Controllers/FriendRequestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Friend;
+use App\Http\Resources\Friend as FriendResource;
+use App\User;
+
+class FriendRequestController extends Controller
+{
+    public function store()
+    {
+        $data = request()->validate([
+            'friend_id' => ''
+        ]);
+        User::find($data['friend_id'])->friends()->attach(auth()->user());
+
+        return new FriendResource(
+            Friend::where('user_id', auth()->user()->id)
+                ->where('friend_id', $data['friend_id'])->first()
+        );
+    }
+}
---
+++ tests/Feature/FriendsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+use App\Friend;
+
+class FriendsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_send_a_friend_request()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+        $anotherUser = factory(User::class)->create();
+
+        $response = $this->post('/api/friend-request', [
+            'friend_id' => $anotherUser->id,
+        ])->assertStatus(200);
+
+        $friendRequest = Friend::first();
+
+        $this->assertNotNull($friendRequest);
+        $this->assertEquals($anotherUser->id, $friendRequest->friend_id);
+        $this->assertEquals($user->id, $friendRequest->user_id);
+        $response->assertJson([
+            'data' => [
+                'type' => 'friend-request',
+                'friend_request_id'  => $friendRequest->id,
+                'attributes' => [
+                    'confirmed_at' => null,
+                ]
+            ],
+            'links' => [
+                'self' => url('/users/' . $anotherUser->id)
+            ]
+        ]);
+    }
+}
