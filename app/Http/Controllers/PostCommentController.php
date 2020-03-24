37730ef025baa0ea353a949fd0be38dc536a96f1
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-03-24 02:20:45 +0000

    aula 48 ok

---
+++ app/Http/Controllers/PostCommentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\Http\Resources\CommentCollection;
+
+class PostCommentController extends Controller
+{
+    public function store(Post $post)
+    {
+        $data = request()->validate([
+            'body' => '',
+        ]);
+
+        $post->comments()->create(array_merge($data, ['user_id' => auth()->user()->id]));
+
+        return new CommentCollection($post->comments);
+    }
+}
