91535a5fdefa4952a217784a200b59a8686d0d49
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-03-31 02:24:10 +0000

    aula 53 ok

--- app/User.php
+++ app/User.php
@@ -51,4 +51,8 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Post::class, 'likes', 'user_id', 'post_id');
     }
+    public function images()
+    {
+        return $this->hasMany(UserImage::class);
+    }
 }
